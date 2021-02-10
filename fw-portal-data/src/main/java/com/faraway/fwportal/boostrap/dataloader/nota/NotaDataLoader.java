7fae5342da3c3903a1ccf635caa8752add090344
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-10 16:07:48 -0300

    Created and implemented NotaDataLoader class

--- fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/nota/NotaDataLoader.java
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/nota/NotaDataLoader.java
@@ -3,11 +3,11 @@ package com.faraway.fwportal.boostrap.dataloader.nota;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.faraway.fwportal.boostrap.builder.Builder;
+import com.faraway.fwportal.boostrap.dataloader.DataLoader;
 import com.faraway.fwportal.model.Empresa;
 import com.faraway.fwportal.model.Nota;
 
-public interface NotaDataLoader extends Builder<Nota> {
+public interface NotaDataLoader extends DataLoader<Nota> {
 
 	NotaDataLoader setEmitente(Empresa args);
 
--- fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/nota/NotaDataLoaderImpl.java
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/nota/NotaDataLoaderImpl.java
@@ -3,56 +3,71 @@ package com.faraway.fwportal.boostrap.dataloader.nota;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.springframework.stereotype.Component;
+
+import com.faraway.fwportal.boostrap.builder.nota.NotaBuilder;
 import com.faraway.fwportal.model.Empresa;
 import com.faraway.fwportal.model.Nota;
+import com.faraway.fwportal.service.NotaCrdService;
 
+@Component
 public class NotaDataLoaderImpl implements NotaDataLoader {
 
+	private final NotaCrdService notaCrudService;
+	private final NotaBuilder notaBuilder;
+
+	public NotaDataLoaderImpl(NotaCrdService notaCrudService, NotaBuilder notaBuilder) {
+		super();
+		this.notaCrudService = notaCrudService;
+		this.notaBuilder = notaBuilder;
+	}
+
 	@Override
-	public Nota buildObject() {
+	public Nota load() {
 		// TODO Auto-generated method stub
-		return null;
+		return notaCrudService.save(notaBuilder.buildObject());
 	}
 
 	@Override
 	public NotaDataLoader setEmitente(Empresa args) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setEmitente(args);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setDestinatario(Empresa args) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setDestinatario(args);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setChave(String chave) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setChave(chave);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setNumero(String numero) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setNumero(numero);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setSerie(String serie) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setSerie(serie);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setEmissao(LocalDate emissao) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setEmissao(emissao);
 		return this;
 	}
 
 	@Override
 	public NotaDataLoader setTotal(BigDecimal total) {
-		// TODO Auto-generated method stub
+		this.notaBuilder.setTotal(total);
+
 		return this;
 	}
 
