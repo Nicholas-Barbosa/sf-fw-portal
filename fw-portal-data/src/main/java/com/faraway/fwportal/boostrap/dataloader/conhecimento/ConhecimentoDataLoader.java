35e9510104433536ddf27bcbca900393e721a2ad
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-11 11:52:53 -0300

    Implemented methods for ConhecimentoDataLoader interface

--- fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/conhecimento/ConhecimentoDataLoader.java
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/conhecimento/ConhecimentoDataLoader.java
@@ -1,8 +1,27 @@
 package com.faraway.fwportal.boostrap.dataloader.conhecimento;
 
+import java.util.Set;
+
+import com.faraway.fwportal.boostrap.builder.conhecimento.ConhecimentoBuilder;
 import com.faraway.fwportal.boostrap.dataloader.DataLoader;
+import com.faraway.fwportal.model.Carga;
+import com.faraway.fwportal.model.Cidade;
 import com.faraway.fwportal.model.Conhecimento;
+import com.faraway.fwportal.model.Entidade;
+import com.faraway.fwportal.model.Imposto;
+import com.faraway.fwportal.model.Nota;
+import com.faraway.fwportal.model.TaxaConhecimento;
 
 public interface ConhecimentoDataLoader extends DataLoader<Conhecimento> {
+
+	ConhecimentoDataLoader setNumeroAndSerieAndChaveAndEmissaoAndTotal(Object... args);
+
+	ConhecimentoDataLoader setCidadeAndCidadeDestino(Cidade... args);
+
+	ConhecimentoDataLoader setEmitenteAndRemetenteAndDestinatario(Entidade... args);
+
+	ConhecimentoBuilder setNotasAndTaxasCollection(Set<Nota> notas, Set<TaxaConhecimento> taxas);
+
+	ConhecimentoBuilder setImpostoAndCarga(Imposto imposto, Carga carga);
 
 }
