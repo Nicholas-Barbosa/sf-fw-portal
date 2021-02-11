8a3112a093ae166189399a09a9c0254c22143189
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-11 11:18:31 -0300

    Added data loader for empresa

---
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/entidade/empresa/EmpesaDataLoaderImpl.java
@@ -0,0 +1,58 @@
+package com.faraway.fwportal.boostrap.dataloader.entidade.empresa;
+
+import org.springframework.stereotype.Component;
+
+import com.faraway.fwportal.boostrap.builder.entidade.empresa.EmpresaBuilder;
+import com.faraway.fwportal.model.Empresa;
+import com.faraway.fwportal.model.Endereco;
+import com.faraway.fwportal.service.EmpresaCrdService;
+
+@Component
+public class EmpesaDataLoaderImpl implements EmpresaDataLoader {
+
+	private final EmpresaBuilder empresaBuilder;
+	private final EmpresaCrdService empresaCrudService;
+
+	public EmpesaDataLoaderImpl(EmpresaBuilder empresaBuilder, EmpresaCrdService empresaCrudService) {
+		super();
+		this.empresaBuilder = empresaBuilder;
+		this.empresaCrudService = empresaCrudService;
+	}
+
+	@Override
+	public Empresa load() {
+		// TODO Auto-generated method stub
+		return empresaCrudService.save(empresaBuilder.buildObject());
+	}
+
+	@Override
+	public EmpresaDataLoader setEndereco(Endereco endereco) {
+		empresaBuilder.setEndereco(endereco);
+		return this;
+	}
+
+	@Override
+	public EmpresaDataLoader setNome(String nome) {
+		empresaBuilder.setNome(nome);
+		return this;
+	}
+
+	@Override
+	public EmpresaDataLoader setCnpj(String cnpj) {
+		empresaBuilder.setCnpj(cnpj);
+		return this;
+	}
+
+	@Override
+	public EmpresaDataLoader setInscEstadual(String inscEstadual) {
+		empresaBuilder.setInscEstadual(inscEstadual);
+		return this;
+	}
+
+	@Override
+	public EmpresaDataLoader setFone(String fone) {
+		empresaBuilder.setFone(fone);
+		return this;
+	}
+
+}
---
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/entidade/empresa/EmpresaDataLoader.java
@@ -0,0 +1,14 @@
+package com.faraway.fwportal.boostrap.dataloader.entidade.empresa;
+
+import com.faraway.fwportal.boostrap.builder.entidade.EntidadeBuilder;
+import com.faraway.fwportal.boostrap.dataloader.DataLoader;
+import com.faraway.fwportal.model.Empresa;
+
+public interface EmpresaDataLoader extends DataLoader<Empresa>, EntidadeBuilder<EmpresaDataLoader> {
+
+	@Override
+	default EmpresaDataLoader setEndereco(Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
