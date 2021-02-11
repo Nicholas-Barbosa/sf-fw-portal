1df7fa6aad13b36c71ff231441b24ac9b7069f4f
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-11 11:56:40 -0300

    ConhecimentoDataLoader interface imports
    Changed imports

--- fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/conhecimento/ConhecimentoDataLoader.java
+++ fw-portal-data/src/main/java/com/faraway/fwportal/boostrap/dataloader/conhecimento/ConhecimentoDataLoader.java
@@ -2,15 +2,8 @@ package com.faraway.fwportal.boostrap.dataloader.conhecimento;
 
 import java.util.Set;
 
-import com.faraway.fwportal.boostrap.builder.conhecimento.ConhecimentoBuilder;
 import com.faraway.fwportal.boostrap.dataloader.DataLoader;
-import com.faraway.fwportal.model.Carga;
-import com.faraway.fwportal.model.Cidade;
-import com.faraway.fwportal.model.Conhecimento;
-import com.faraway.fwportal.model.Entidade;
-import com.faraway.fwportal.model.Imposto;
-import com.faraway.fwportal.model.Nota;
-import com.faraway.fwportal.model.TaxaConhecimento;
+import com.faraway.fwportal.model.*;
 
 public interface ConhecimentoDataLoader extends DataLoader<Conhecimento> {
 
