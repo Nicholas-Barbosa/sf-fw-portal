4a5f458641bffb9308c4437c37cfe438041ef4aa
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-15 23:42:07 -0300

    Create EnderecoBuilder.java

---
+++ fw-portal-data/src/main/java/com/faraway/fwportal/model/builder/EnderecoBuilder.java
@@ -0,0 +1,53 @@
+package com.faraway.fwportal.model.builder;
+
+import com.faraway.fwportal.model.domain.Cidade;
+import com.faraway.fwportal.model.domain.Endereco;
+
+public class EnderecoBuilder implements Builder<EnderecoBuilder, Endereco> {
+
+	private String logradouro, numero, cep, bairro, pais;
+	private Cidade cidade;
+
+	@Override
+	public EnderecoBuilder builder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Endereco build() {
+		// TODO Auto-generated method stub
+		return new Endereco(logradouro, numero, cep, cidade, bairro, pais);
+	}
+
+	public EnderecoBuilder cidade(Cidade cidade) {
+		this.cidade = new Cidade(cidade);
+		return this;
+	}
+
+	public EnderecoBuilder logradouro(String logradouro) {
+		this.logradouro = logradouro;
+		return this;
+	}
+
+	public EnderecoBuilder numero(String numero) {
+		this.numero = numero;
+		return this;
+	}
+
+	public EnderecoBuilder cep(String cep) {
+		this.cep = cep;
+		return this;
+	}
+
+	public EnderecoBuilder bairro(String bairro) {
+		this.bairro = bairro;
+		return this;
+	}
+
+	public EnderecoBuilder pais(String pais) {
+		this.pais = pais;
+		return this;
+	}
+
+}
